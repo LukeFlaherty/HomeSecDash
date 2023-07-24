63d8a96f0b3df0902724526a080520167992cd15
Luke <flaherty.luke@yahoo.com>
2023-07-23 21:45:15 -0400

    get on the board

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function IndexPage() {
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
-      <p>This is wehatever I wabnt</p>
+      <p>This is where your dashboard would be</p>
     </Layout>
   );
 }
