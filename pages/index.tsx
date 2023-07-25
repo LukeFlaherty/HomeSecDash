07781485e5afab448f0b97d7701b18dfe6759b77
Luke <flaherty.luke@yahoo.com>
2023-07-24 23:12:37 -0400

    for today but more laster

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>My Lazr Dashboard</h1>
       <p>This is where your dashboard would be</p>
     </Layout>
   );
